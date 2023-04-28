ba0e3f674224892a80b497ec0aa10ccf31e58268
Niklas Grebe <thyphoon@5on5.org>
2023-04-28 10:00:57 +0200

    Switch to gpt 3.5, adjust token length

--- src/main.ts
+++ src/main.ts
@@ -132,9 +132,9 @@ async function getAIResponse(prompt: string): Promise<Array<{
   reviewComment: string;
 }> | null> {
   const queryConfig = {
-    model: "gpt-4",
+    model: "gpt-3.5-turbo", //gpt-4
     temperature: 0.2,
-    max_tokens: 700,
+    max_tokens: 2048, //700
     top_p: 1,
     frequency_penalty: 0,
     presence_penalty: 0,
