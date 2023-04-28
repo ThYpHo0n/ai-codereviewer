10d1bf672d3396e6f5f046d11ddcdd2a120b9f91
Niklas Grebe <niklas.grebe@bytro.com>
2023-04-28 11:38:31 +0200

    Update dist

--- dist/index.js
+++ dist/index.js
@@ -151,9 +151,9 @@ function getAIResponse(prompt) {
     var _a, _b;
     return __awaiter(this, void 0, void 0, function* () {
         const queryConfig = {
-            model: "gpt-4",
+            model: "gpt-3.5-turbo",
             temperature: 0.2,
-            max_tokens: 700,
+            max_tokens: 2048,
             top_p: 1,
             frequency_penalty: 0,
             presence_penalty: 0,
